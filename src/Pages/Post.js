3dd14b910faacab2113b4367cea9bf9b4c719904
NBinte <nuzhatbintei@gmail.com>
2022-05-18 14:54:46 +0600

    Add useLocation hook

--- src/Pages/Post.js
+++ src/Pages/Post.js
@@ -4,16 +4,16 @@ import { useLocation, useParams } from "react-router-dom";
 const Post = () => {
   const { id } = useParams();
 
-  //   const query = new URLSearchParams(useLocation().search);
+  const query = new URLSearchParams(useLocation().search);
 
-  //   const first = query.get("first");
-  //   const last = query.get("last");
+  const first = query.get("first");
+  const last = query.get("last");
 
   return (
     <>
       <h2>Id is = {id} </h2>
-      {/* <h2>First Name is = {first} </h2>
-      <h2>Last name is = {last} </h2> */}
+      <h2>First Name is = {first} </h2>
+      <h2>Last name is = {last} </h2>
     </>
   );
 };
